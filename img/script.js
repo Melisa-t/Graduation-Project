0d997ea1e01eebd1b30b6ef4a9c4e1d630c4742b
Melisa-t <melisatormac55@gmail.com>
2024-09-27 02:34:43 +0300

    Add days, hours, minutes, seconds left

--- img/script.js
+++ img/script.js
@@ -9,3 +9,20 @@ let seconds  = document.getElementById("seconds")
 const currentYear = new Date().getFullYear();
 const newTime = new Date(` April 4 ${currentYear + 1} 00:00:00:00 `)
 
+function countdownTimer(){
+    const currentTime = new Date(); 
+    const difference = newTime - currentTime;
+
+    const d = Math.floor(difference / 1000 / 60 / 60 / 24);
+    const h = Math.floor(difference / 1000 / 60 / 60 % 24);
+    const m = Math.floor(difference / 1000 / 60 % 60);
+    const s = Math.floor(difference / 1000 % 60 );
+
+    days.innerHTML = d;
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+
+}
+
+countdownTimer()
