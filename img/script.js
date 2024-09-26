7c8f75e95b39cf79c219eddc8edf4962a639ee61
Melisa-t <melisatormac55@gmail.com>
2024-09-27 02:15:22 +0300

    Plan JS file and get elements

---
+++ img/script.js
@@ -0,0 +1,8 @@
+let days = document.getElementById("days")
+let hours = document.getElementById("hours")
+let minutes = document.getElementById("minutes")
+let seconds  = document.getElementById("seconds")
+
+// set a variable for current time and new time, 
+// then calculate the time
+
