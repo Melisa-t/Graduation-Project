e67ee919dbabd460c071744db2f2add96a770789
Melisa-t <melisatormac55@gmail.com>
2024-09-27 02:17:04 +0300

    Create variables for current time and target date

--- img/script.js
+++ img/script.js
@@ -6,3 +6,6 @@ let seconds  = document.getElementById("seconds")
 // set a variable for current time and new time, 
 // then calculate the time
 
+const currentYear = new Date().getFullYear();
+const newTime = new Date(` April 4 ${currentYear + 1} 00:00:00:00 `)
+
